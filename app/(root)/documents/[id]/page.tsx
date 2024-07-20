212d423c460aeac3a6f31b0fe099c05ea21ba869
Tharuka Deshan <tharukaurbanco@gmail.com>
2024-07-20 10:58:20 +0500

    inital

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Editor } from "@/components/editor/Editor";
+import React from "react";
+
+const DocumentPage = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
+
+export default DocumentPage;
